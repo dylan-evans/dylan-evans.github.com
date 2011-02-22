b1bfcea3885170579c75cd380c4270b1c84847b1
Dylan Evans <dylan@contentfree.info>
2011-02-22 15:33:28 +0000

    Started adding blogging

---
+++ blog.py
@@ -0,0 +1,17 @@
+"""\
+Create a blog post without even thinking about the date.
+
+This program will:
+* Create a file named by the -t option with year-month-day
+* Add front matter
+* Open the file with the default $EDITOR or vi
+* When the editor exits it will check if the file has changed
+* If it has not it will delete the file and exit
+* Add the file with git and commit according to the -m option
+
+This program currently
+* Prints TODO and exits
+"""
+
+print "TODO"
+#success
