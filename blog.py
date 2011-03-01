005be79bfceda2fa3b423635e058ff49290b6a41
Dylan Evans <dylan@contentfree.info>
2011-03-01 16:41:16 +0000

    Add option and experimental Popen

--- blog.py
+++ blog.py
@@ -22,20 +22,25 @@ import stat
 import re
 import datetime
 from optparse import OptionParser
+from subprocess import Popen, PIPE
 
 def safe_title(title):
+    "Convert the title string into a safe format"
     t = re.sub('[^A-Za-z0-9\s]', '', title) #Remove all symbols
     t = re.sub('\s+', '-', t.strip().lower())
     return t[0:50] if len(t) > 50 else t
 
 def file_name(title):
+    "Create the date coded filename"
     return datetime.date.today().strftime('%Y-%m-%d') + '-' + safe_title(title)
 
 def create_front_matter(title, published=True):
+    "Create a front matter for the post"
     return ("---\n" "layout: post\n" "title:%s\n" "published: %s\n" "---\n") % \
           (title, 'true' if published else 'false')
           
 def create_post(title, published):
+    "Create a post file and write the front matter"
     filename = os.path.join('_posts', file_name(title) + '.md')
     print 'Creating file: %s' % filename
     try:
@@ -48,15 +53,21 @@ def create_post(title, published):
     return filename
     
 if __name__ == '__main__':
+    default_editor = os.environ['EDITOR'] if os.environ.has_key('EDITOR') else '/usr/bin/vi'
     opt = OptionParser()
     opt.add_option('-t', '--title', help='Blog Title')
     opt.add_option('-m', '--message', help='Commit message', default='Add new blog' )
-    opt.add_option('-P', '--no-publish', action='store_true', default=False)
+    opt.add_option('-P', '--no-publish', action='store_true', default=False, help='Do not publish post')
+    opt.add_option('-e', '--editor', default=default_editor)
     (options, args) = opt.parse_args()
     
     if not options.title:
         print("The -t TITLE argument is required")
         sys.exit()
     
-    create_post(options.title, not opt.no_publish)
+    filename = create_post(options.title, not options.no_publish)
+    # I have to get back to that
+    #p = Popen([options.editor, filename], stdin=sys.stdout, stdout=sys.stdout)
+    #p.wait()
+    
     
