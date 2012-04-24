fbe45a43735888e939d55e341fd6efe832e75bed
Dylan Evans <dylane@dje.me>
2012-04-24 21:46:37 +1000

    Moves app code to the Life function and removes console.log for compatibility with IE.

--- life/life.js
+++ life/life.js
@@ -2,33 +2,51 @@
 jQuery(document).ready(function($) {
 	'use strict';
 	var draw = false;
-	console.log("Loading life");
+	
+	var config= {
+		CELL_SIZE: 10,
+		FPS: 20,
+		width: 60,
+		height: 40
+	};
+
+	//console.log("Loading life");
 	
 	/* Grid object */
 	function Grid(height, width) {
+		
 		this.height = height;
 		this.width = width;
-		this.cells = new Array();
+		
+		// This is the state of the grid when it is started
 		this.first = null;
 		
+		// The current grid
+		this.grid = new Array();
+		
+		// Change the Grid back to the start state
 		this.reset = function() {
-			this.cells = this.first;
+			this.grid = this.first;
 			this.first = null;
 		};
 		
+		// Clear the grid
 		this.clear = function() {
-			this.cells = new Array();
+			this.grid = new Array();
 			this.first = null;
 		};
 		
+		// Get the state of a cell
 		this.get = function(row, col) {
-			return this.cells[row + (col * height)];
+			return this.grid[row + (col * height)];
 		};
 		
+		// Set the state of a cell
 		this.set = function(row, col, obj) {
-			this.cells[row + (col * height)] = obj;
+			this.grid[row + (col * height)] = obj;
 		};
 		
+		// Count the number of cells adjacent to a cell which are set 
 		this.count = function(centerRow, centerCol) {
 			var r, c, count = 0;
 			for(r = -1; r < 2; r += 1) {
@@ -47,11 +65,12 @@ jQuery(document).ready(function($) {
 			return count;
 		};
 		
+		// Advance the grid one tick
 		this.step = function(render, ctx) {
 			var row, col, changes = 0;
 			var next = new Array();
 			if(!this.first) {
-				this.first = this.cells;
+				this.first = this.grid;
 			}
 			for(row = 0; row < this.height; row += 1) {
 				for(col = 0; col < this.width; col += 1) {
@@ -79,183 +98,195 @@ jQuery(document).ready(function($) {
 					next[row + (col * height)] = alive;
 				}
 			}
-			this.cells = next;
+			this.grid = next;
 			return changes;
 		};
 		
+		// Set the entire grid to a random state
 		this.rand = function() {
 			this.clear();
 			var row, col;
 			for(row = 0; row < this.height; row += 1) {
 				for(col = 0; col < this.width; col += 1) {
-					this.cells[row + (col * height)] = (Math.floor(Math.random() * 100) % 2);
-					//console.log("Number:" + this.cells[row + (col * height)]);
+					this.grid[row + (col * height)] = (Math.floor(Math.random() * 100) % 2);
+					//console.log("Number:" + this.grid[row + (col * height)]);
 				}
 			}
 		};
 	}
 	
-	/* Configs */
-	var mode = 'draw';
-	var world = $('#world');
-	var prevX = 0, prevY = 0;
-	var FPS = 10;
-	var CELL_SIZE = 5;
-	var config= {
-		CELL_SIZE: 2,
-		FPS: 20
-	};
-	var grid = new Grid(world[0].height / CELL_SIZE, world[0].width / CELL_SIZE);
-		
-	function renderCell(row, col, alive, ctx) {
-		if(alive) {
-			ctx.fillStyle = 'rgb(0, 150, 25)';
-		} else {
-			
-			if((row & 1 && col & 1) || (!(row & 1) && !(col & 1))) {
-				ctx.fillStyle = 'white';
+	function Life(world, grid, config) {
+		// Render a single cell on the grid
+		this.renderCell = function(row, col, alive, ctx) {
+			if(alive) {
+				ctx.fillStyle = 'rgb(0, 150, 25)';
 			} else {
-				ctx.fillStyle = 'rgb(200, 200, 200)';
-			}		
-		}
-		ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-	}
-	
-	function renderStep() {
-		var ctx = world[0].getContext('2d');
-		var changes = grid.step(renderCell, ctx);
-		ctx.stroke();
-		return changes;
-	}
-	
-	function renderGrid() {
-		var row, col;
-		var ctx = world[0].getContext('2d');
-		ctx.beginPath();
-		for(row = 0; row < grid.height; row += 1) {
-			for(col = 0; col < grid.width; col += 1) {
-				if(grid.get(row, col)) {
-					ctx.fillStyle = 'rgb(0, 150, 25)';
+				
+				if((row & 1 && col & 1) || (!(row & 1) && !(col & 1))) {
+					ctx.fillStyle = 'white';
 				} else {
-					
-					if((row & 1 && col & 1) || (!(row & 1) && !(col & 1))) {
-						ctx.fillStyle = 'white';
+					ctx.fillStyle = 'rgb(200, 200, 200)';
+				}		
+			}
+			ctx.fillRect(col * config.CELL_SIZE, row * config.CELL_SIZE, 
+				config.CELL_SIZE, config.CELL_SIZE);
+		}
+
+		// Call step and render each cell which has changed
+		this.renderStep = function() {
+			var ctx = world[0].getContext('2d');
+			var changes = grid.step(this.renderCell, ctx);
+			ctx.stroke();
+			return changes;
+		}
+
+		// Render every cell in the grid
+		this.renderGrid = function() {
+			var row, col;
+			var ctx = world[0].getContext('2d');
+			ctx.beginPath();
+			for(row = 0; row < grid.height; row += 1) {
+				for(col = 0; col < grid.width; col += 1) {
+					if(grid.get(row, col)) {
+						ctx.fillStyle = 'rgb(0, 150, 25)';
 					} else {
-						ctx.fillStyle = 'rgb(200, 200, 200)';
-					}		
+						
+						if((row & 1 && col & 1) || (!(row & 1) && !(col & 1))) {
+							ctx.fillStyle = 'white';
+						} else {
+							ctx.fillStyle = 'rgb(200, 200, 200)';
+						}		
+					}
+					ctx.fillRect(col * config.CELL_SIZE, row * config.CELL_SIZE, config.CELL_SIZE, config.CELL_SIZE);
 				}
-				ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+			}
+			ctx.stroke();
+		}
+	}
+	
+	// If world is defined run the script
+	var world = $('#world');
+	if(world !== undefined) {
+		var mode = 'draw';
+		var grid = new Grid(world[0].height / config.CELL_SIZE, world[0].width / config.CELL_SIZE);
+		var life = new Life(world, grid, config);
+		
+		life.renderGrid();
+		
+		// The timeout id
+		var stepID = 0;
+		
+		// Step through
+		function step() {
+			//console.log('step');
+			if(!life.renderStep()) {
+				pause();
+			}
+			if(mode === 'running') {
+				stepID = window.setTimeout(step, 1000 / config.FPS);
 			}
 		}
-		ctx.stroke();
-	}
-	
-	renderGrid();
-	
-	world.click(function(e) {
-		if(mode === 'draw') {
-			var offset = world.offset();
-			var row = (e.pageY - offset.top);
-			row = (row - (row % 5)) / 5;
-			var col = (e.pageX - offset.left);
-			col = (col - (col % 5)) / 5;
-			console.log("Plotting row: " + row + " col: " + col);
-			grid.set(row, col, true);
-			renderGrid();
+		
+		// Switch to run mode
+		function run() {
+			mode = 'running';
+			$('#start-button').attr('value', 'Pause');
+			$('#step-button').attr('disabled', 'disabled');
+			step();
 		}
-	});
-	
-	world.mousedown(function(e) {
-		if(mode === 'draw') {
-			mode = 'select';
-			var offset = world.offset();
-			prevX = (e.pageX - offset.left);
-			prevY = (e.pageY - offset.top);
-		}
-	});
-	
-	world.mousemove(function(e) {
-		if(mode === 'select') {
-			var offset = world.offset();
-			var row = (e.pageY - offset.top);
-			row = (row - (row % CELL_SIZE)) / CELL_SIZE;
-			var col = (e.pageX - offset.left);
-			col = (col - (col % CELL_SIZE)) / CELL_SIZE;
-			console.log("Plotting row: " + row + " col: " + col);
-			grid.set(row, col, true);
-			renderGrid();
-			prevX = (e.pageX - offset.left);
-			prevY = (e.pageY - offset.top);
-		}
-	});
-	
-	world.mouseup(function(e) {
-		if(mode === 'select') {
+		
+		// Stop running
+		function pause() {
 			mode = 'draw';
+			window.clearTimeout(stepID);
+			$('#start-button').attr('value', 'Start');
+			$('#step-button').removeAttr('disabled');
 		}
-	});
-	
-	var stepID = 0;
-	function step() {
-		console.log('step');
-		if(!renderStep()) {
+		
+		// Any click on the grid
+		world.click(function(e) {
+			if(mode === 'draw') {
+				var offset = world.offset();
+				//Find the row and column
+				var row = (e.pageY - offset.top);
+				row = (row - (row % config.CELL_SIZE)) / config.CELL_SIZE;
+				var col = (e.pageX - offset.left);
+				col = (col - (col % config.CELL_SIZE)) / config.CELL_SIZE;
+				//console.log("Plotting row: " + row + " col: " + col);
+				grid.set(row, col, true);
+				life.renderGrid();
+			}
+		});
+		
+		// 
+		world.mousedown(function(e) {
+			if(mode === 'draw') {
+				mode = 'select';
+				var offset = world.offset();
+				prevX = (e.pageX - offset.left);
+				prevY = (e.pageY - offset.top);
+			}
+		});
+		
+		world.mousemove(function(e) {
+			if(mode === 'select') {
+				var offset = world.offset();
+				var row = (e.pageY - offset.top);
+				row = (row - (row % CELL_SIZE)) / CELL_SIZE;
+				var col = (e.pageX - offset.left);
+				col = (col - (col % CELL_SIZE)) / CELL_SIZE;
+				//console.log("Plotting row: " + row + " col: " + col);
+				grid.set(row, col, true);
+				life.renderGrid();
+				prevX = (e.pageX - offset.left);
+				prevY = (e.pageY - offset.top);
+			}
+		});
+		
+		world.mouseup(function(e) {
+			if(mode === 'select') {
+				mode = 'draw';
+			}
+		});
+		
+		$('#start-button').click(function(e) {
+			if(mode === 'running') {
+				pause();
+			} else {
+				run();
+			}
+		});
+		
+		
+		$('#step-button').click(function(e) {
+			if(mode === 'draw') {
+				life.renderStep();
+			}
+		});
+		
+		$('#reset-button').click(function() {
+			grid.reset();
+			life.renderGrid();
+		});
+		
+		// Clear button 
+		$('#clear-button').click(function() {
+			if(mode !== 'draw') {
+				pause();
+			}
+			grid.clear();
+			life.renderGrid();
+		});
+		
+		// Random button sets 
+		$('#random-button').click(function() {
 			pause();
-		}
-		if(mode === 'running') {
-			stepID = window.setTimeout(step, 1000 / FPS);
-		}
+			if(mode == 'draw') {
+				grid.rand();
+				life.renderGrid();
+				run();
+			}
+		});
 	}
-	
-	function run() {
-		mode = 'running';
-		$('#start-button').attr('value', 'Pause');
-		$('#step-button').attr('disabled', 'disabled');
-		step();
-	}
-	
-	function pause() {
-		mode = 'draw';
-		window.clearTimeout(stepID);
-		$('#start-button').attr('value', 'Start');
-		$('#step-button').removeAttr('disabled');
-	}
-	
-	$('#start-button').click(function(e) {
-		if(mode === 'running') {
-			pause();
-		} else {
-			run();
-		}
-	});
-	
-	
-	$('#step-button').click(function(e) {
-		if(mode === 'draw') {
-			renderStep();
-		}
-	});
-	
-	$('#reset-button').click(function() {
-		grid.reset();
-		renderGrid();
-	});
-	
-	$('#clear-button').click(function() {
-		if(mode !== 'draw') {
-			pause();
-		}
-		grid.clear();
-		renderGrid();
-	});
-	
-	$('#random-button').click(function() {
-		pause();
-		if(mode == 'draw') {
-			grid.rand();
-			renderGrid();
-			run();
-		}
-	});
-	
-	console.log("Loaded");
+	//console.log("Loaded");
 });
